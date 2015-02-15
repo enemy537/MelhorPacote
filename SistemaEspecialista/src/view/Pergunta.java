2ab84716605267c6acec68bb14037be91df610f6
ArnobioJunior <arnobiojroliveira@gmail.com>
2015-02-15 10:44:47 -0300

    Seletor FC

--- SistemaEspecialista/src/view/Pergunta.java
+++ SistemaEspecialista/src/view/Pergunta.java
@@ -1,10 +1,15 @@
 package view;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.Lighting;
 import javafx.scene.layout.BorderPane;
@@ -22,7 +27,7 @@ public class Pergunta extends BorderPane{
 	private Stage stage;
 	private Group rootGroup;
 	private Scene scene;
-	private Text text, ajuda;
+	private Text text, fc, ajuda;
 	private Button ok;
 	private TextField campo;
 	
@@ -31,12 +36,31 @@ public class Pergunta extends BorderPane{
 	public Pergunta(String pergunta){
 		stage = new Stage();
 		rootGroup = new Group();
-		scene = new Scene(rootGroup, 500, 200, Color.WHITESMOKE);
+		scene = new Scene(rootGroup, 600, 300, Color.WHITESMOKE);
 		
 		stage.initModality(Modality.WINDOW_MODAL);
 		
 		campo = new TextField();
 		campo.setPromptText("SIM ou NAO");
+		
+		fc = new Text("Fator de Confiaça:  ");
+		
+		final Slider slider = new Slider();
+		slider.setMin(0);
+        slider.setMax(100);
+        
+        final ProgressIndicator pi = new ProgressIndicator(0);
+ 
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+                pi.setProgress(new_val.doubleValue()/50);
+            }
+        });
+        
+        HBox hbox = new HBox(20);
+        hbox.getChildren().addAll(fc,slider,pi);
+		
 		ok = new Button("OK");
 		
 		ok.setOnAction(new EventHandler<ActionEvent>() {
@@ -72,7 +96,7 @@ public class Pergunta extends BorderPane{
 	    text.setFont(Font.font(Font.getDefault().getFamily(), 20));
 	    
 	    VBox vbox = new VBox(20);
-	    vbox.getChildren().addAll(text,ajuda,campo,ok);
+	    vbox.getChildren().addAll(text,ajuda,campo,hbox,ok);
 	    vbox.setAlignment(Pos.CENTER);
 	    vbox.setTranslateX(160);
 	    
